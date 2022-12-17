e3cac595716ce32cc27c022257897c7bccb0ee10
StevenK418 <stevenkelly418@gmail.com>
2022-12-17 17:53:06 +0000

    => Adding initial Button flash template code to the repo.

---
+++ app/src/main/java/com/example/memorygameapp/Timer.java
@@ -0,0 +1,34 @@
+package com.example.memorygameapp;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
+
+public class Timer {
+
+//    CountDownTimer ct = new CountDownTimer(6000,  1500) {
+//
+//        public void onTick(long millisUntilFinished) {
+//            //mTextField.setText("seconds remaining: " + millisUntilFinished / 1500);
+//            oneButton();
+//            //here you can have your logic to set text to edittext
+//        }
+//
+//        public void onFinish() {
+//            //mTextField.setText("done!");
+//            // we now have the game sequence
+//
+//            for (int i = 0; i< arrayIndex; i++)
+//                Log.d("game sequence", String.valueOf(gameSequence[i]));
+//            // start next activity
+//
+//            // put the sequence into the next activity
+//            // stack overglow https://stackoverflow.com/questions/3848148/sending-arrays-with-intent-putextra
+//            //Intent i = new Intent(A.this, B.class);
+//            //i.putExtra("numbers", array);
+//            //startActivity(i);
+//
+//            // start the next activity
+//            // int[] arrayB = extras.getIntArray("numbers");
+//        }
+//    };
+}
